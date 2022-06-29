dceb4daffa5f6445d2f64597da430cbd28602550
maxmay94 <maxmay94@gmail.com>
2022-06-28 21:44:54 -0400

    solve 7

--- project-euler.js
+++ project-euler.js
@@ -138,3 +138,29 @@ const sumSquareDifference = () => {
 }
 // console.log(sumSquareDifference())
 // ANSWER: 25164150
+
+
+/*
+  7. By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+  What is the 10 001st prime number?
+*/
+const lateStagePrime = () => {
+
+  function isPrime(n) {
+    if(n === 1) return false
+    let sqrtN = Math.floor(Math.sqrt(n))
+    for(let i = 2; i <= sqrtN; i++){
+      if(n % i === 0) return false
+    }
+    return true
+  }
+
+  let primes = []
+  for(let i = 0; primes.length <= 10001; i++) {
+    if(isPrime(i)) primes.push(i)
+  }
+  return Math.max(...primes)
+}
+// console.log(lateStagePrime())
+// ANSWER: 104743
+
