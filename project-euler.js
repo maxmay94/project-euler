a26b0075db1a37e8169db2ead2bedbd9c1a0cd78
maxmay94 <maxmay94@gmail.com>
2022-07-03 11:45:50 -0400

    solve 15

--- project-euler.js
+++ project-euler.js
@@ -590,6 +590,17 @@ const longsetCollatzSequence = () => {
 
 
 
+/*
+  15.Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
+
+  How many such routes are there through a 20×20 grid?
+*/
+const latticePaths = () => {
+  return factorialize(40) / (factorialize(20) * factorialize(20))
+}
+// console.log(latticePaths())
+// ANSWER: 137846528820
+
 /* ------------------------------------------ HELPER FUNCTIONS ------------------------------------------ */
 function isPrime(n) {
   if(n === 1) return false
@@ -598,4 +609,10 @@ function isPrime(n) {
     if(n % i === 0) return false
   }
   return true
+}
+
+function factorialize(n) {
+  if(n < 0) return -1
+  else if(n ===0) return 1
+  else return (n * factorialize(n - 1))
 }
